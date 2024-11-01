9887a8722d769273896864d19a591b67e065b4be
Triud45 <vfletes45@gmail.com>
2024-11-01 15:38:19 -0400

    Create getHumanChoice function
    Allows the user to enter rock, paper, scissors into the code

--- java.js
+++ java.js
@@ -3,5 +3,13 @@ function getComputerChoice()
     return Math.floor(Math.random() * 3);
 }
 
-let computerChoice = getComputerChoice()
-alert(computerChoice);
+function getHumanChoice(choice)
+{
+    choice = choice.toLowerCase();
+    alert(choice);
+}
+
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice(prompt("Please choose Rock, Paper, or Scissors and type it below!"));
+
+
