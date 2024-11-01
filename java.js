fff91050b614c38f01c6d3ae5b0de4be9f5cc273
Triud45 <vfletes45@gmail.com>
2024-11-01 17:36:00 -0400

    Add playRound function and edit getHumanChoice function to return number instead of starting
    This is so that the program compare human choice with the computer choice easier
    and also so that the program can evaluate who won the round

--- java.js
+++ java.js
@@ -1,3 +1,9 @@
+/* 0 = rock  1 = paper  2 = scissors*/
+let humanScore = 0;
+let computerScore = 0;
+let humanChoice;
+let computerChoice
+
 function getComputerChoice() 
 {
     return Math.floor(Math.random() * 3);
@@ -5,11 +11,66 @@ function getComputerChoice()
 
 function getHumanChoice(choice)
 {
+    let rps;
     choice = choice.toLowerCase();
-    alert(choice);
+    if(choice === 'rock')
+    { 
+        rps = 0;
+        return rps;
+    }
+    else if(choice === 'paper')
+    {
+        rps = 1;
+        return rps;
+    }
+    else if (choice === 'scissors' || choice === 'scissor')
+    {
+        rps = 2;
+        return rps;
+    }
+    else
+    {
+        rps = 0;
+        alert('You entered an invalid choice, rock is chosen by defualt');
+        return rps;
+    }
 }
 
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice(prompt("Please choose Rock, Paper, or Scissors and type it below!"));
+function playRound(humanChoice, computerChoice)
+{
+    if(humanChoice === computerChoice)
+    {
+        alert('Tie! Try again');
+    }
+    else if(humanChoice === 0 && computerChoice === 1)
+    {
+        alert('You lose! Paper beats Rock');
+        computerScore++;
+    }
+    else if(humanChoice === 1 && computerChoice === 2)
+    {
+        alert('You lose! Scissors beats Paper');
+        computerScore++;
+    }
+    else if(humanChoice === 2 && computerChoice === 0)
+    {
+        alert('You lose! Rock beats Scissors');
+        computerScore++;
+    }
+    else
+    {
+        alert('You win!');
+        humanScore++;
+    }
+}
+
+function playGame()
+{
+    humanChoice = getHumanChoice(prompt('Please choose Rock, Paper, or Scissors and enter it below'));
+
+}
+
+console.log(getHumanChoice(prompt('Please choose Rock, Paper, or Scissors and enter it below')));
+
 
 
